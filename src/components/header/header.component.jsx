dc23a38bf49c28e014d2268f6694513e74f6f3c3
lewis rosenberg-smith <lrosenbergsmith@gmail.com>
2021-04-19 17:00:52 +1200

    added a page and component for sign in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,13 +8,17 @@ import "./header.styles.scss";
 const Header = () => (
   <div className="header">
     <Link className="logo-container" to="/">
-      <Logo className="logo"></Logo>
+      <Logo className="logo" />
     </Link>
-    <div className="option">
-      <Link to="/shop">SHOP</Link>
-      <Link to="/shop">CONTACT</Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
     </div>
   </div>
 );
 
-export default Header
+export default Header;
