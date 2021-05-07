b05d65cd19e5590825edab0f6c4478a7ced63a0b
Lewis Rosenberg-Smith <lrosenbergsmith@gmail.com>
2021-05-07 15:02:12 +1200

    created cart-item component and connected our cart dropdown with cartItems reducer. rebuilt node modules

--- src/components/cart-icon/cart.icon.component.jsx
+++ src/components/cart-icon/cart.icon.component.jsx
@@ -10,11 +10,11 @@ import "./cart.icon.styles.scss";
 const CartIcon = ({ toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count"></span>
+    <span className="item-count">0</span>
   </div>
 );
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
